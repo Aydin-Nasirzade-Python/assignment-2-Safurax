572cd810b96171df6b387973a4afbc2751617a09
Safurax <165558499+Safurax@users.noreply.github.com>
2024-04-07 16:35:41 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -4,29 +4,29 @@ def main():
   x=int(input('Enter the year [ex. 2021]: '))
   if x>=0:
     if x%12==8:
-        print(f'{x} is the year of Dragon')
+        print(f'{x} is the year of the Dragon')
     elif x%12==9:
-        print(f'{x} is the year of Snake')
+        print(f'{x} is the year of the Snake')
     elif x%12==10:
-        print(f'{x} is the year of Horse')
+        print(f'{x} is the year of the Horse')
     elif x%12==11:
-        print(f'{x} is the year of Sheep')
+        print(f'{x} is the year of the Sheep')
     elif x%12==7:
-        print(f'{x} is the year of Hare')
+        print(f'{x} is the year of the Hare')
     elif x%12==1:
-        print(f'{x} is the year of Rooster')
+        print(f'{x} is the year of the Rooster')
     elif x%12==2:
-        print(f'{x} is the year of Dog')
+        print(f'{x} is the year of the Dog')
     elif x%12==3:
-        print(f'{x} is the year of Pig')
+        print(f'{x} is the year of the Pig')
     elif x%12==4:
-        print(f'{x} is the year of Rat')
+        print(f'{x} is the year of the Rat')
     elif x%12==5:
-        print(f'{x} is the year of Ox')
+        print(f'{x} is the year of the Ox')
     elif x%12==6:
-        print(f'{x} is the year of Tiger')
+        print(f'{x} is the year of the Tiger')
     else:
-        print(f'{x} is the year of Monkey')
+        print(f'{x} is the year of the Monkey')
   else:
     print('Invalid year!')
   pass
